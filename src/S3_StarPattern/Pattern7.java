7bb86e1abc100ff2bfaa6dbcf6ff3d8807387d76
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-02-11 08:32:14 +0530

    11th Feb 2026

---
+++ src/S3_StarPattern/Pattern7.java
@@ -0,0 +1,21 @@
+package S3_StarPattern;
+public class Pattern7
+{
+    public static void main(String[] args)
+    {
+        //***
+        //**
+        //*
+
+        int star=3;
+        for(int i=1; i<=3; i++)
+        {
+            for(int j=1; j<=star; j++)
+            {
+                System.out.print("*");
+            }
+            star--;                        //4
+            System.out.println();
+        }
+    }
+}
