2e780803d0737860ff570d22a4f3e3d3f851e423
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-05-13 08:17:48 +0530

    13th May 2026

---
+++ src/S3_LogicalProgram/DiffBetween_final_finally_finalize.java
@@ -0,0 +1,29 @@
+package S3_LogicalProgram;
+public class DiffBetween_final_finally_finalize
+{
+    public static void main(String[] args)
+    {
+        final int a=10;
+//        a=20;             //can't perform re-initialze
+        System.out.println(a);
+
+
+        String s1="abcd";
+        try
+        {
+            System.out.println(s1.charAt(5));   //risky code
+        }
+        catch(StringIndexOutOfBoundsException e)
+        {
+            System.out.println("StringIndexOutOfBounds Exception handled");
+        }
+        finally
+        {
+            System.out.println("running finally block");
+        }
+
+
+        // finalize()
+    }
+}
+
---
+++ src/S3_LogicalProgram/diffBetweenThrowAndThowsKeyword.java
@@ -0,0 +1,22 @@
+package S3_LogicalProgram;
+
+
+public class diffBetweenThrowAndThowsKeyword
+{
+    public static void main(String[] args) throws Exception {
+
+        Thread.sleep(2000);
+
+
+        checkAge(5);
+
+    }
+
+    public static void checkAge(int age) throws Exception {
+        if (age < 18)
+        {
+            throw new Exception("Age must be 18 or older.");
+        }
+    }
+
+}
